190b9202d50fcd6a67f1a697f5f717fcdb8739b8
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2022-10-31 19:26:03 -0300

    Pesquisa Component 2

--- src/componentes/Pesquisa/pesquisa.js
+++ src/componentes/Pesquisa/pesquisa.js
@@ -1,10 +1,34 @@
+import Input from "../BarraDePesquisa/BarraDePesquisa";
+import styled from "styled-components";
+
+const PesquisaContainer = styled.section`
+        background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+        color: #FFF;
+        text-align: center;
+        padding: 85px 0;
+        height: 270px;
+        width: 100%;
+`
+
+const Titulo = styled.h2`
+        color: #FFF;
+        font-size: 36px;
+        text-align: center;
+        width: 100%;
+`
+
+const SubTitulo = styled.h3`
+        font-size: 16px;
+        font-weight: 500;
+        margin-bottom: 40px;
+`
 function Pesquisa() {
     return (
-        <section>
-            <h2>Já sabe por onde começar?</h2>
-            <h3>Encontre seu livro em nossa estante.</h3>
-            <input placeholder="Digite sua próxima leitura"></input>
-        </section>
+        <PesquisaContainer>
+            <Titulo>Já sabe por onde começar?</Titulo>
+            <SubTitulo>Encontre seu livro em nossa estante</SubTitulo>
+            <Input placeholder="Digite sua próxima leitura"/>
+        </PesquisaContainer>
     )
 }
 
