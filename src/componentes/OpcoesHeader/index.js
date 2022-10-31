7c8849129879298b29d446273aa9f57de3bcd096
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2022-10-31 18:49:51 -0300

    Adicionando Styled Components 2

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,14 +1,30 @@
+import styled from "styled-components";
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const Opcao = styled.li`
+        font-size: 16px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        height: 100%;
+        padding: 0 5px;
+        cursor: pointer;
+        min-width: 120px;
+        margin-right: 10px;
+`
+const Opcoes = styled.ul`
+        display: flex;
+        
+`
+
 function OpcoesHeader() {
     return (
-        <ul className='opcoes'>
-            { textoOpcoes.map((texto) => (
-                <li className='opcao'>
-                    <p>{ texto }</p>
-                </li>
-            )) }
-        </ul>
+        <Opcoes>
+            { textoOpcoes.map( (texto) => (
+                <Opcao><p>{texto}</p></Opcao>
+            ) ) }
+        </Opcoes>
     )
 }
 export default OpcoesHeader
